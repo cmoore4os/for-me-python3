709025e89228effd2c9b9793b54f60bdadb771b9
cmoore4os <cm4glos@gmail.com>
2018-04-17 10:47:50 -0400

    Edit TIME_ZONE in settings.py

--- django_tuts/first_django_project/first_django_project/settings.py
+++ django_tuts/first_django_project/first_django_project/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd party apps
+
+    # our apps
+    #'polls'
 ]
 
 MIDDLEWARE = [
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
